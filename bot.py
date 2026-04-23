ca38a4d942af69b3465073c19bf6cf39d82b0d9c
Jamboss <j.ccradnoy@gmail.com>
2026-04-23 16:12:06 +0500

    Add files via upload

--- bot.py
+++ bot.py
@@ -21,6 +21,7 @@ from telegram.ext import (
     Application,
     ApplicationBuilder,
     CallbackQueryHandler,
+    ChatMemberHandler,
     CommandHandler,
     ContextTypes,
     MessageHandler,
